c6b27c4d33536275d0e1db596ae9c7c595dbf576
Ðóàù James Xarca <66143527+xBarkDog@users.noreply.github.com>
2022-09-20 14:12:05 -0500

    test server list command

--- main.py
+++ main.py
@@ -28,6 +28,24 @@ listMessages = {}
 async def on_ready():
   print("Initialized")
 
+class OwnerCommands(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("OwnerCommands Is Ready")
+
+    @commands.command()
+    async def servers(self, ctx):
+        activeservers = client.guilds
+        for guild in activeservers:
+            await ctx.send(guild.name)
+            print(guild.name)
+
+def setup(client):
+    client.add_cog(OwnerCommands(client))
 
 @client.command()
 async def dl(ctx, arg):
